920e8917e54a3dd78fcad636647a7cb85a82c09e
Thanh Son <ngthanhson0701@gmail.com>
2020-08-31 08:21:59 +0700

    Almost done

---
+++ PHP-noAlpha.py
@@ -0,0 +1,51 @@
+import sys
+
+arguments = sys.argv
+
+for index in range(len(arguments)):
+    if(arguments[index] == "--in"):
+        file_in  = arguments[index + 1]
+    if(arguments[index] == "--out"):
+        file_out = arguments[index + 1]
+
+file_input = open(file_in, 'r')
+php_code = file_input.read().upper()
+
+obfuscated = """$_="";$____="";$__=[]."_";$___=$__["."=="+"];$_____=$__["."=="+"];"""
+
+php_code_function_list =  []
+php_code_param_list = []
+php_code_function = php_code[:php_code.find('(')]
+php_code_param = php_code[php_code.find('(') + 2 : php_code.find(')') -1]
+
+php_code_function_list[:] = php_code_function
+php_code_param_list[:] = php_code_param
+
+print(php_code_function_list)
+print(php_code_param_list)
+
+for c in php_code_param_list:
+    if c.isalpha():   
+        obfuscated += '$_____++;'*(ord(c) - ord('A'))
+        obfuscated += '$____=$____.$_____;'
+        obfuscated += '$_____=$__["."=="+"];'
+    else:
+        if (c == '"'):
+            obfuscated += "$____.='{0}';".format('\"')
+        else:
+            obfuscated += '$____.="{0}";'.format(c)
+
+for c in php_code_function_list:
+    if c.isalpha():   
+        obfuscated += '$___++;'*(ord(c) - ord('A'))
+        obfuscated += '$_=$_.$___;'
+        obfuscated += '$___=$__["."=="+"];'
+    else:
+        if (c == '"'):
+            obfuscated += "$_=$_.'{0}';".format('\"')
+        else:
+            obfuscated += '$_=$_."{0}";'.format(c)
+
+obfuscated += "$_($____);"
+with open(file_out, 'w') as file_output:
+    file_output.write(obfuscated)
